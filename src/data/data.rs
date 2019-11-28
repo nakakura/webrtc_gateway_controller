2a4fc6d0b364f02eb95547c9dc3efd44ec8838cd
Toshiya Nakakura <nakakura@me.com>
2019-11-28 18:09:40 +0900

    create_data_connection

--- src/data/data.rs
+++ src/data/data.rs
@@ -7,3 +7,56 @@ pub struct CreatedResponse {
     pub ip_v4: Option<String>,
     pub ip_v6: Option<String>,
 }
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialOrd, PartialEq)]
+pub struct CreateDataConnectionQuery {
+    pub peer_id: String,
+    pub token: String,
+    pub options: Option<DataConnectionParameters>,
+    pub target_id: String,
+    pub params: DataId,
+    pub redirect_params: Option<RedirectParams>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialOrd, PartialEq)]
+#[allow(non_snake_case)]
+pub struct DataConnectionParameters {
+    pub metadata: String,
+    pub serialization: String,
+    pub dcInit: DcInit,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialOrd, PartialEq)]
+#[allow(non_snake_case)]
+pub struct DcInit {
+    pub ordered: bool,
+    pub maxPacketLifeTime: usize,
+    pub maxRetransmits: usize,
+    pub protocol: String,
+    pub negotiated: bool,
+    pub id: usize,
+    pub priority: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialOrd, PartialEq)]
+pub struct DataId {
+    pub data_id: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialOrd, PartialEq)]
+pub struct CreateDataConnectionResponse {
+    pub command_type: String,
+    pub params: DataConnectionId,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialOrd, PartialEq)]
+pub struct DataConnectionId {
+    pub data_connection_id: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialOrd, PartialEq)]
+pub struct RedirectParams {
+    pub ip_v4: Option<String>,
+    pub ip_v6: Option<String>,
+    pub port: u16,
+}
--- src/data/mod.rs
+++ src/data/mod.rs
@@ -93,6 +93,57 @@ pub async fn delete_data(base_url: &str, data_id: &str) -> Result<(), error::Err
     }
 }
 
+/// This function access to the POST /data/connections endpoint.
+/// The API returns 202 Accepted, when a WebRTC Gateway succeed to start calling
+/// It returns 400, 403, 404, 405, 406, 408 to show errors.
+/// http://35.200.46.204/#/2.data/data_connections_create
+pub async fn create_data_connection(
+    base_url: &str,
+    params: &CreateDataConnectionQuery,
+) -> Result<CreateDataConnectionResponse, error::ErrorEnum> {
+    let api_url = format!("{}/data/connections", base_url);
+    let res = Client::new().post(&api_url).json(params).send().await?;
+
+    match res.status() {
+        http::status::StatusCode::ACCEPTED => res
+            .json::<CreateDataConnectionResponse>()
+            .await
+            .map_err(Into::into),
+        http::status::StatusCode::BAD_REQUEST => res
+            .json::<PeerErrorResponse>()
+            .await
+            .map_err(Into::into)
+            .and_then(|response: PeerErrorResponse| {
+                let message = response
+                    .params
+                    .errors
+                    .iter()
+                    .fold("recv message".to_string(), |sum, acc| {
+                        format!("{}\n{}", sum, acc.message)
+                    });
+                Err(error::ErrorEnum::create_myerror(&message))
+            }),
+        http::status::StatusCode::FORBIDDEN => {
+            Err(error::ErrorEnum::create_myerror("recv Forbidden"))
+        }
+        http::status::StatusCode::NOT_FOUND => {
+            Err(error::ErrorEnum::create_myerror("recv Not Found"))
+        }
+        http::status::StatusCode::METHOD_NOT_ALLOWED => {
+            Err(error::ErrorEnum::create_myerror("recv Method Not Allowed"))
+        }
+        http::status::StatusCode::NOT_ACCEPTABLE => {
+            Err(error::ErrorEnum::create_myerror("recv Not Acceptable"))
+        }
+        http::status::StatusCode::REQUEST_TIMEOUT => {
+            Err(error::ErrorEnum::create_myerror("recv RequestTimeout"))
+        }
+        _ => {
+            unreachable!();
+        }
+    }
+}
+
 #[cfg(test)]
 mod test_create_data {
     use serde_json::json;
@@ -509,6 +560,341 @@ mod test_delete_data {
 
         let addr = format!("http://{}", server.addr());
         let task = super::delete_data(&addr, data_id);
+        let result = task.await.err().expect("parse error");
+        if let error::ErrorEnum::MyError { error: _e } = result {
+        } else {
+            unreachable!();
+        }
+    }
+}
+
+mod test_create_data_connection {
+    use serde_json::json;
+
+    use crate::data::data::*;
+    use crate::error;
+    use crate::helper::*;
+
+    /// The API returns 204 No Content, when a WebRTC Gateway succeed to delete a Data Object.
+    /// http://35.200.46.204/#/2.data/data
+    #[tokio::test]
+    async fn test_create_data_connection_202() {
+        let peer_id = "peer_id";
+        let token = "test-token";
+        let target_id = "target_id";
+        let data_id = "da-test";
+
+        let server = server::http(move |mut req| {
+            async move {
+                if req.uri() == "/data/connections" && req.method() == reqwest::Method::POST {
+                    let mut full: Vec<u8> = Vec::new();
+                    while let Some(item) = req.body_mut().next().await {
+                        full.extend(&*item.unwrap());
+                    }
+                    let _peer_options: CreateDataConnectionQuery =
+                        serde_json::from_slice(&full).expect("PeerOptions parse error");
+                    let json = json!({
+                        "command_type": "PEERS_CONNECT",
+                        "params": {
+                            "data_connection_id": "dc-test"
+                        }
+                    });
+                    http::Response::builder()
+                        .status(hyper::StatusCode::ACCEPTED)
+                        .header("Content-type", "application/json")
+                        .body(hyper::Body::from(json.to_string()))
+                        .unwrap()
+                } else {
+                    unreachable!();
+                }
+            }
+        });
+
+        let data_id = DataId {
+            data_id: data_id.to_string(),
+        };
+        let query = CreateDataConnectionQuery {
+            peer_id: peer_id.to_string(),
+            token: token.to_string(),
+            options: None,
+            target_id: target_id.to_string(),
+            params: data_id,
+            redirect_params: None,
+        };
+
+        let addr = format!("http://{}", server.addr());
+        let task = super::create_data_connection(&addr, &query);
+        let result = task.await.expect("parse error");
+        assert_eq!(result.params.data_connection_id, "dc-test");
+    }
+
+    /// It returns 400 to show errors
+    /// http://35.200.46.204/#/2.data/data_connections_create
+    #[tokio::test]
+    async fn test_create_data_connection_400() {
+        let peer_id = "peer_id";
+        let token = "test-token";
+        let target_id = "target_id";
+        let data_id = "da-test";
+
+        let server = server::http(move |req| {
+            async move {
+                if req.uri() == "/data/connections" && req.method() == reqwest::Method::POST {
+                    let json = json!({
+                        "command_type": "DATA_CONNECTION_CREATE",
+                        "params": {
+                            "errors": [
+                                {
+                                    "field": "field",
+                                    "message": "something happened"
+                                }
+                            ]
+                        }
+                    });
+                    http::Response::builder()
+                        .status(hyper::StatusCode::BAD_REQUEST)
+                        .header("Content-type", "application/json")
+                        .body(hyper::Body::from(json.to_string()))
+                        .unwrap()
+                } else {
+                    unreachable!();
+                }
+            }
+        });
+
+        let addr = format!("http://{}", server.addr());
+        let data_id = DataId {
+            data_id: data_id.to_string(),
+        };
+        let query = CreateDataConnectionQuery {
+            peer_id: peer_id.to_string(),
+            token: token.to_string(),
+            options: None,
+            target_id: target_id.to_string(),
+            params: data_id,
+            redirect_params: None,
+        };
+        let task = super::create_data_connection(&addr, &query);
+        let result = task.await.err().expect("parse error");
+        if let error::ErrorEnum::MyError { error: _e } = result {
+        } else {
+            unreachable!();
+        }
+    }
+
+    /// It returns 403 to show errors
+    /// http://35.200.46.204/#/2.data/data_connections_create
+    #[tokio::test]
+    async fn test_create_data_connection_403() {
+        let peer_id = "peer_id";
+        let token = "test-token";
+        let target_id = "target_id";
+        let data_id = "da-test";
+
+        let server = server::http(move |req| {
+            async move {
+                if req.uri() == "/data/connections" && req.method() == reqwest::Method::POST {
+                    let json = json!({});
+                    http::Response::builder()
+                        .status(hyper::StatusCode::FORBIDDEN)
+                        .header("Content-type", "application/json")
+                        .body(hyper::Body::from(json.to_string()))
+                        .unwrap()
+                } else {
+                    unreachable!();
+                }
+            }
+        });
+
+        let addr = format!("http://{}", server.addr());
+        let data_id = DataId {
+            data_id: data_id.to_string(),
+        };
+        let query = CreateDataConnectionQuery {
+            peer_id: peer_id.to_string(),
+            token: token.to_string(),
+            options: None,
+            target_id: target_id.to_string(),
+            params: data_id,
+            redirect_params: None,
+        };
+        let task = super::create_data_connection(&addr, &query);
+        let result = task.await.err().expect("parse error");
+        if let error::ErrorEnum::MyError { error: _e } = result {
+        } else {
+            unreachable!();
+        }
+    }
+
+    /// It returns 404 to show errors
+    /// http://35.200.46.204/#/2.data/data_connections_create
+    #[tokio::test]
+    async fn test_create_data_connection_404() {
+        let peer_id = "peer_id";
+        let token = "test-token";
+        let target_id = "target_id";
+        let data_id = "da-test";
+
+        let server = server::http(move |req| {
+            async move {
+                if req.uri() == "/data/connections" && req.method() == reqwest::Method::POST {
+                    let json = json!({});
+                    http::Response::builder()
+                        .status(hyper::StatusCode::NOT_FOUND)
+                        .header("Content-type", "application/json")
+                        .body(hyper::Body::from(json.to_string()))
+                        .unwrap()
+                } else {
+                    unreachable!();
+                }
+            }
+        });
+
+        let addr = format!("http://{}", server.addr());
+        let data_id = DataId {
+            data_id: data_id.to_string(),
+        };
+        let query = CreateDataConnectionQuery {
+            peer_id: peer_id.to_string(),
+            token: token.to_string(),
+            options: None,
+            target_id: target_id.to_string(),
+            params: data_id,
+            redirect_params: None,
+        };
+        let task = super::create_data_connection(&addr, &query);
+        let result = task.await.err().expect("parse error");
+        if let error::ErrorEnum::MyError { error: _e } = result {
+        } else {
+            unreachable!();
+        }
+    }
+
+    /// It returns 405 to show errors
+    /// http://35.200.46.204/#/2.data/data_connections_create
+    #[tokio::test]
+    async fn test_create_data_connection_405() {
+        let peer_id = "peer_id";
+        let token = "test-token";
+        let target_id = "target_id";
+        let data_id = "da-test";
+
+        let server = server::http(move |req| {
+            async move {
+                if req.uri() == "/data/connections" && req.method() == reqwest::Method::POST {
+                    let json = json!({});
+                    http::Response::builder()
+                        .status(hyper::StatusCode::METHOD_NOT_ALLOWED)
+                        .header("Content-type", "application/json")
+                        .body(hyper::Body::from(json.to_string()))
+                        .unwrap()
+                } else {
+                    unreachable!();
+                }
+            }
+        });
+
+        let addr = format!("http://{}", server.addr());
+        let data_id = DataId {
+            data_id: data_id.to_string(),
+        };
+        let query = CreateDataConnectionQuery {
+            peer_id: peer_id.to_string(),
+            token: token.to_string(),
+            options: None,
+            target_id: target_id.to_string(),
+            params: data_id,
+            redirect_params: None,
+        };
+        let task = super::create_data_connection(&addr, &query);
+        let result = task.await.err().expect("parse error");
+        if let error::ErrorEnum::MyError { error: _e } = result {
+        } else {
+            unreachable!();
+        }
+    }
+
+    /// It returns 406 to show errors
+    /// http://35.200.46.204/#/2.data/data_connections_create
+    #[tokio::test]
+    async fn test_create_data_connection_406() {
+        let peer_id = "peer_id";
+        let token = "test-token";
+        let target_id = "target_id";
+        let data_id = "da-test";
+
+        let server = server::http(move |req| {
+            async move {
+                if req.uri() == "/data/connections" && req.method() == reqwest::Method::POST {
+                    let json = json!({});
+                    http::Response::builder()
+                        .status(hyper::StatusCode::NOT_ACCEPTABLE)
+                        .header("Content-type", "application/json")
+                        .body(hyper::Body::from(json.to_string()))
+                        .unwrap()
+                } else {
+                    unreachable!();
+                }
+            }
+        });
+
+        let addr = format!("http://{}", server.addr());
+        let data_id = DataId {
+            data_id: data_id.to_string(),
+        };
+        let query = CreateDataConnectionQuery {
+            peer_id: peer_id.to_string(),
+            token: token.to_string(),
+            options: None,
+            target_id: target_id.to_string(),
+            params: data_id,
+            redirect_params: None,
+        };
+        let task = super::create_data_connection(&addr, &query);
+        let result = task.await.err().expect("parse error");
+        if let error::ErrorEnum::MyError { error: _e } = result {
+        } else {
+            unreachable!();
+        }
+    }
+
+    /// It returns 408 to show errors
+    /// http://35.200.46.204/#/2.data/data_connections_create
+    #[tokio::test]
+    async fn test_create_data_connection_408() {
+        let peer_id = "peer_id";
+        let token = "test-token";
+        let target_id = "target_id";
+        let data_id = "da-test";
+
+        let server = server::http(move |req| {
+            async move {
+                if req.uri() == "/data/connections" && req.method() == reqwest::Method::POST {
+                    let json = json!({});
+                    http::Response::builder()
+                        .status(hyper::StatusCode::REQUEST_TIMEOUT)
+                        .header("Content-type", "application/json")
+                        .body(hyper::Body::from(json.to_string()))
+                        .unwrap()
+                } else {
+                    unreachable!();
+                }
+            }
+        });
+
+        let addr = format!("http://{}", server.addr());
+        let data_id = DataId {
+            data_id: data_id.to_string(),
+        };
+        let query = CreateDataConnectionQuery {
+            peer_id: peer_id.to_string(),
+            token: token.to_string(),
+            options: None,
+            target_id: target_id.to_string(),
+            params: data_id,
+            redirect_params: None,
+        };
+        let task = super::create_data_connection(&addr, &query);
         let result = task.await.err().expect("parse error");
         if let error::ErrorEnum::MyError { error: _e } = result {
         } else {

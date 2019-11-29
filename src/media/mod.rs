a60ad8c5dc216ae43187ed3d5f6113dea342a989
Toshiya Nakakura <nakakura@me.com>
2019-11-29 16:26:39 +0900

    add pli

--- src/media/mod.rs
+++ src/media/mod.rs
@@ -3,6 +3,7 @@ mod data;
 use reqwest;
 use reqwest::Client;
 
+use crate::data::data::RedirectParams;
 use crate::error;
 use crate::peer::data::PeerErrorResponse;
 use data::*;
@@ -315,6 +316,54 @@ pub async fn answer(
             }),
         http::status::StatusCode::FORBIDDEN => {
             Err(error::ErrorEnum::create_myerror("recv Forbidden"))
+        }
+        http::status::StatusCode::METHOD_NOT_ALLOWED => {
+            Err(error::ErrorEnum::create_myerror("recv Method Not Allowed"))
+        }
+        http::status::StatusCode::NOT_ACCEPTABLE => {
+            Err(error::ErrorEnum::create_myerror("recv Not Acceptable"))
+        }
+        http::status::StatusCode::REQUEST_TIMEOUT => {
+            Err(error::ErrorEnum::create_myerror("recv RequestTimeout"))
+        }
+        _ => {
+            unreachable!();
+        }
+    }
+}
+
+/// Fn pli access to the POST /media/connections/{media_connection_id}/pli endpoint.
+/// If the API returns values with 201 Accepted, it returns ()
+/// If server returns 400, 403, 405, 406, 408, it returns error
+/// http://35.200.46.204/#/3.media/media_connection_pli
+pub async fn pli(
+    base_url: &str,
+    media_connection_id: &str,
+    params: &RedirectParams,
+) -> Result<(), error::ErrorEnum> {
+    let api_url = format!("{}/media/connections/{}/pli", base_url, media_connection_id);
+    let res = Client::new().post(&api_url).json(params).send().await?;
+    match res.status() {
+        http::status::StatusCode::CREATED => Ok(()),
+        http::status::StatusCode::BAD_REQUEST => res
+            .json::<PeerErrorResponse>()
+            .await
+            .map_err(Into::into)
+            .and_then(|response: PeerErrorResponse| {
+                let message = response
+                    .params
+                    .errors
+                    .iter()
+                    .fold("recv message".to_string(), |sum, acc| {
+                        format!("{}\n{}", sum, acc.message)
+                    });
+                Err(error::ErrorEnum::create_myerror(&message))
+            }),
+        http::status::StatusCode::FORBIDDEN => {
+            Err(error::ErrorEnum::create_myerror("recv Forbidden"))
+        }
+        http::status::StatusCode::NOT_FOUND => {
+            Err(error::ErrorEnum::create_myerror("recv Not Found"))
         }
         http::status::StatusCode::METHOD_NOT_ALLOWED => {
             Err(error::ErrorEnum::create_myerror("recv Method Not Allowed"))
@@ -1959,6 +2008,288 @@ mod test_answer {
             redirect_params: None,
         };
         let task = super::answer(&addr, media_connection_id, &params);
+        let result = task.await.err().expect("event parse error");
+        if let error::ErrorEnum::MyError { error: _e } = result {
+        } else {
+            unreachable!();
+        }
+    }
+}
+
+#[cfg(test)]
+mod test_pli {
+    use serde_json::json;
+
+    use crate::data::data::RedirectParams;
+    use crate::error;
+    use crate::helper::*;
+
+    /// Fn pli access to the POST /media/connections/{media_connection_id}/pli endpoint.
+    /// If the API returns values with 201 Accepted, it returns ()
+    /// http://35.200.46.204/#/3.media/media_connection_pli
+    #[tokio::test]
+    async fn recv_202() {
+        let media_connection_id = "mc-test";
+
+        let server = server::http(move |req| {
+            async move {
+                let uri = format!("/media/connections/{}/pli", media_connection_id);
+                if req.uri().to_string() == uri && req.method() == reqwest::Method::POST {
+                    let json = json!({});
+                    http::Response::builder()
+                        .status(hyper::StatusCode::CREATED)
+                        .header("Content-type", "application/json")
+                        .body(hyper::Body::from(json.to_string()))
+                        .unwrap()
+                } else {
+                    unreachable!();
+                }
+            }
+        });
+
+        let addr = format!("http://{}", server.addr());
+        let params = RedirectParams {
+            port: 10001,
+            ip_v4: Some("127.0.0.1".to_string()),
+            ip_v6: None,
+        };
+
+        let task = super::pli(&addr, media_connection_id, &params);
+        let result = task.await.expect("event parse error");
+        assert_eq!(result, ());
+    }
+
+    /// Fn pli access to the POST /media/connections/{media_connection_id}/pli endpoint.
+    /// If server returns 400, it returns error
+    /// http://35.200.46.204/#/3.media/media_connection_plir
+    #[tokio::test]
+    async fn recv_400() {
+        let media_connection_id = "mc-test";
+
+        let server = server::http(move |req| {
+            async move {
+                let uri = format!("/media/connections/{}/pli", media_connection_id);
+                if req.uri().to_string() == uri && req.method() == reqwest::Method::POST {
+                    let json = json!({
+                        "command_type": "MEDIA_CONNECTION_PLI",
+                        "params": {
+                            "errors": [
+                                {
+                                    "field": "media_id",
+                                    "message": "media_id field is not specified"
+                                }
+                            ]
+                        }
+                    });
+                    http::Response::builder()
+                        .status(hyper::StatusCode::BAD_REQUEST)
+                        .header("Content-type", "application/json")
+                        .body(hyper::Body::from(json.to_string()))
+                        .unwrap()
+                } else {
+                    unreachable!();
+                }
+            }
+        });
+
+        let addr = format!("http://{}", server.addr());
+        let params = RedirectParams {
+            port: 10001,
+            ip_v4: Some("127.0.0.1".to_string()),
+            ip_v6: None,
+        };
+
+        let task = super::pli(&addr, media_connection_id, &params);
+        let result = task.await.err().expect("event parse error");
+        if let error::ErrorEnum::MyError { error: _e } = result {
+        } else {
+            unreachable!();
+        }
+    }
+
+    /// Fn pli access to the POST /media/connections/{media_connection_id}/pli endpoint.
+    /// If server returns 403, it returns error
+    /// http://35.200.46.204/#/3.media/media_connection_plir
+    #[tokio::test]
+    async fn recv_403() {
+        let media_connection_id = "mc-test";
+
+        let server = server::http(move |req| {
+            async move {
+                let uri = format!("/media/connections/{}/pli", media_connection_id);
+                if req.uri().to_string() == uri && req.method() == reqwest::Method::POST {
+                    let json = json!({});
+                    http::Response::builder()
+                        .status(hyper::StatusCode::FORBIDDEN)
+                        .header("Content-type", "application/json")
+                        .body(hyper::Body::from(json.to_string()))
+                        .unwrap()
+                } else {
+                    unreachable!();
+                }
+            }
+        });
+
+        let addr = format!("http://{}", server.addr());
+        let params = RedirectParams {
+            port: 10001,
+            ip_v4: Some("127.0.0.1".to_string()),
+            ip_v6: None,
+        };
+
+        let task = super::pli(&addr, media_connection_id, &params);
+        let result = task.await.err().expect("event parse error");
+        if let error::ErrorEnum::MyError { error: _e } = result {
+        } else {
+            unreachable!();
+        }
+    }
+
+    /// Fn pli access to the POST /media/connections/{media_connection_id}/pli endpoint.
+    /// If server returns 404, it returns error
+    /// http://35.200.46.204/#/3.media/media_connection_plir
+    #[tokio::test]
+    async fn recv_404() {
+        let media_connection_id = "mc-test";
+
+        let server = server::http(move |req| {
+            async move {
+                let uri = format!("/media/connections/{}/pli", media_connection_id);
+                if req.uri().to_string() == uri && req.method() == reqwest::Method::POST {
+                    let json = json!({});
+                    http::Response::builder()
+                        .status(hyper::StatusCode::NOT_FOUND)
+                        .header("Content-type", "application/json")
+                        .body(hyper::Body::from(json.to_string()))
+                        .unwrap()
+                } else {
+                    unreachable!();
+                }
+            }
+        });
+
+        let addr = format!("http://{}", server.addr());
+        let params = RedirectParams {
+            port: 10001,
+            ip_v4: Some("127.0.0.1".to_string()),
+            ip_v6: None,
+        };
+
+        let task = super::pli(&addr, media_connection_id, &params);
+        let result = task.await.err().expect("event parse error");
+        if let error::ErrorEnum::MyError { error: _e } = result {
+        } else {
+            unreachable!();
+        }
+    }
+
+    /// Fn pli access to the POST /media/connections/{media_connection_id}/pli endpoint.
+    /// If server returns 405, it returns error
+    /// http://35.200.46.204/#/3.media/media_connection_plir
+    #[tokio::test]
+    async fn recv_405() {
+        let media_connection_id = "mc-test";
+
+        let server = server::http(move |req| {
+            async move {
+                let uri = format!("/media/connections/{}/pli", media_connection_id);
+                if req.uri().to_string() == uri && req.method() == reqwest::Method::POST {
+                    let json = json!({});
+                    http::Response::builder()
+                        .status(hyper::StatusCode::METHOD_NOT_ALLOWED)
+                        .header("Content-type", "application/json")
+                        .body(hyper::Body::from(json.to_string()))
+                        .unwrap()
+                } else {
+                    unreachable!();
+                }
+            }
+        });
+
+        let addr = format!("http://{}", server.addr());
+        let params = RedirectParams {
+            port: 10001,
+            ip_v4: Some("127.0.0.1".to_string()),
+            ip_v6: None,
+        };
+
+        let task = super::pli(&addr, media_connection_id, &params);
+        let result = task.await.err().expect("event parse error");
+        if let error::ErrorEnum::MyError { error: _e } = result {
+        } else {
+            unreachable!();
+        }
+    }
+
+    /// Fn pli access to the POST /media/connections/{media_connection_id}/pli endpoint.
+    /// If server returns 406, it returns error
+    /// http://35.200.46.204/#/3.media/media_connection_plir
+    #[tokio::test]
+    async fn recv_406() {
+        let media_connection_id = "mc-test";
+
+        let server = server::http(move |req| {
+            async move {
+                let uri = format!("/media/connections/{}/pli", media_connection_id);
+                if req.uri().to_string() == uri && req.method() == reqwest::Method::POST {
+                    let json = json!({});
+                    http::Response::builder()
+                        .status(hyper::StatusCode::NOT_ACCEPTABLE)
+                        .header("Content-type", "application/json")
+                        .body(hyper::Body::from(json.to_string()))
+                        .unwrap()
+                } else {
+                    unreachable!();
+                }
+            }
+        });
+
+        let addr = format!("http://{}", server.addr());
+        let params = RedirectParams {
+            port: 10001,
+            ip_v4: Some("127.0.0.1".to_string()),
+            ip_v6: None,
+        };
+
+        let task = super::pli(&addr, media_connection_id, &params);
+        let result = task.await.err().expect("event parse error");
+        if let error::ErrorEnum::MyError { error: _e } = result {
+        } else {
+            unreachable!();
+        }
+    }
+
+    /// Fn pli access to the POST /media/connections/{media_connection_id}/pli endpoint.
+    /// If server returns 408, it returns error
+    /// http://35.200.46.204/#/3.media/media_connection_plir
+    #[tokio::test]
+    async fn recv_408() {
+        let media_connection_id = "mc-test";
+
+        let server = server::http(move |req| {
+            async move {
+                let uri = format!("/media/connections/{}/pli", media_connection_id);
+                if req.uri().to_string() == uri && req.method() == reqwest::Method::POST {
+                    let json = json!({});
+                    http::Response::builder()
+                        .status(hyper::StatusCode::REQUEST_TIMEOUT)
+                        .header("Content-type", "application/json")
+                        .body(hyper::Body::from(json.to_string()))
+                        .unwrap()
+                } else {
+                    unreachable!();
+                }
+            }
+        });
+
+        let addr = format!("http://{}", server.addr());
+        let params = RedirectParams {
+            port: 10001,
+            ip_v4: Some("127.0.0.1".to_string()),
+            ip_v6: None,
+        };
+
+        let task = super::pli(&addr, media_connection_id, &params);
         let result = task.await.err().expect("event parse error");
         if let error::ErrorEnum::MyError { error: _e } = result {
         } else {

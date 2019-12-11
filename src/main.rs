c87b79ecfefb87f6d92ceb39461e6042b8fff003
Toshiya Nakakura <nakakura@me.com>
2019-12-11 13:04:36 +0900

    arrange functions handling peers

--- src/main.rs
+++ src/main.rs
@@ -6,32 +6,20 @@ use webrtc_gateway_controller::data::formats::CreatedResponse;
 use webrtc_gateway_controller::peer::formats::*;
 use webrtc_gateway_controller::*;
 
-#[cfg(not(test))]
-#[allow(dead_code)]
-#[tokio::main]
-async fn main() {
-    env_logger::init();
+struct PeerEventObservers {
+    on_open_tx: Vec<Sender<PeerOpenEvent>>,
+    on_call_tx: Vec<Sender<PeerCallEvent>>,
+    on_connect_tx: Vec<Sender<PeerConnectionEvent>>,
+    on_close_tx: Vec<Sender<PeerCloseEvent>>,
+    on_error_tx: Vec<Sender<PeerErrorEvent>>,
+}
 
-    // setup callback functions for PeerObject
+// create peer object and notify each event to observers
+async fn peer_runner(observers: PeerEventObservers) {
     // FIRES when GET /peer/{peer_id}/events returns OPEN event
+    // redirect the event message to observers
     let (on_open_tx, on_open_rx) = channel::<peer::formats::PeerOpenEvent>(0);
-    // FIRES when GET /peer/{peer_id}/events returns CALL event
-    let (on_call_tx, on_call_rx) = channel::<peer::formats::PeerCallEvent>(0);
-    // FIRES when GET /peer/{peer_id}/events returns CONNECT event
-    let (on_connect_tx, on_connect_rx) = channel::<peer::formats::PeerConnectionEvent>(0);
-    // FIRES when GET /peer/{peer_id}/events returns CLOSE event
-    let (on_close_tx, on_close_rx) = channel::<peer::formats::PeerCloseEvent>(0);
-    // FIRES when GET /peer/{peer_id}/events returns ERROR event
-    let (on_error_tx, on_error_rx) = channel::<peer::formats::PeerErrorEvent>(0);
-
-    // On Open Event is used in some flows, so redirect it
-    // For data connection
-    let (sub_on_open_tx_1, sub_on_open_rx_1) = channel::<peer::formats::PeerOpenEvent>(0);
-    // For media connection
-    let (sub_on_open_tx_2, _sub_on_open_rx_2) = channel::<peer::formats::PeerOpenEvent>(0);
-    let tx_array = vec![sub_on_open_tx_1, sub_on_open_tx_2];
-    // On Open Event Redirect
-    let on_open_future = on_open_rx.fold(tx_array, |mut sum, item| {
+    let on_open_notify_future = on_open_rx.fold(observers.on_open_tx, |mut sum, item| {
         async move {
             for tx in sum.iter_mut() {
                 let _ = tx.send(item.clone()).await;
@@ -39,18 +27,86 @@ async fn main() {
             sum
         }
     });
+    tokio::spawn(on_open_notify_future.map(|_| ()));
 
-    let on_call_future = on_call_rx.for_each(on_peer_call);
-    let on_close_future = on_close_rx.for_each(on_peer_close);
-    let on_error_future = on_error_rx.for_each(on_peer_error);
-    // Start subscribing each events
-    tokio::spawn(on_open_future.map(|_| ()));
-    tokio::spawn(on_call_future);
-    tokio::spawn(on_close_future);
-    tokio::spawn(on_error_future);
+    // FIRES when GET /peer/{peer_id}/events returns CALL event
+    let (on_call_tx, on_call_rx) = channel::<peer::formats::PeerCallEvent>(0);
+    let on_call_notify_future = on_call_rx.fold(observers.on_call_tx, |mut sum, item| {
+        async move {
+            for tx in sum.iter_mut() {
+                let _ = tx.send(item.clone()).await;
+            }
+            sum
+        }
+    });
+    tokio::spawn(on_call_notify_future.map(|_| ()));
+
+    // FIRES when GET /peer/{peer_id}/events returns CONNECT event
+    let (on_connect_tx, on_connect_rx) = channel::<peer::formats::PeerConnectionEvent>(0);
+    let on_connect_notify_future = on_connect_rx.fold(observers.on_connect_tx, |mut sum, item| {
+        async move {
+            for tx in sum.iter_mut() {
+                let _ = tx.send(item.clone()).await;
+            }
+            sum
+        }
+    });
+    tokio::spawn(on_connect_notify_future.map(|_| ()));
+
+    // FIRES when GET /peer/{peer_id}/events returns CLOSE event
+    let (on_close_tx, on_close_rx) = channel::<peer::formats::PeerCloseEvent>(0);
+    let on_close_notify_future = on_close_rx.fold(observers.on_close_tx, |mut sum, item| {
+        async move {
+            for tx in sum.iter_mut() {
+                let _ = tx.send(item.clone()).await;
+            }
+            sum
+        }
+    });
+    tokio::spawn(on_close_notify_future.map(|_| ()));
+    // FIRES when GET /peer/{peer_id}/events returns ERROR event
+    let (on_error_tx, on_error_rx) = channel::<peer::formats::PeerErrorEvent>(0);
+    let on_error_notify_future = on_error_rx.fold(observers.on_error_tx, |mut sum, item| {
+        async move {
+            for tx in sum.iter_mut() {
+                let _ = tx.send(item.clone()).await;
+            }
+            sum
+        }
+    });
+    tokio::spawn(on_error_notify_future.map(|_| ()));
+
+    let _ = peer::peer_create_and_listen_events(
+        &*BASE_URL,
+        &*PEER_ID,
+        true,
+        Some(on_open_tx),
+        Some(on_call_tx),
+        Some(on_connect_tx),
+        Some(on_close_tx),
+        Some(on_error_tx),
+    )
+    .await;
+}
+
+#[cfg(not(test))]
+#[allow(dead_code)]
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let (on_open_tx, on_open_rx) = channel::<peer::formats::PeerOpenEvent>(0);
+    let (on_connect_tx, on_connect_rx) = channel::<peer::formats::PeerConnectionEvent>(0);
+    let observers = PeerEventObservers {
+        on_open_tx: vec![on_open_tx],
+        on_call_tx: vec![],
+        on_connect_tx: vec![on_connect_tx],
+        on_close_tx: vec![],
+        on_error_tx: vec![],
+    };
 
     if *CONNECT_FLAG {
-        let connect_future = sub_on_open_rx_1.for_each(|event: PeerOpenEvent| {
+        let connect_future = on_open_rx.for_each(|event: PeerOpenEvent| {
             async move {
                 let (on_connection_open_tx, on_connection_open_rx) = channel::<String>(0);
                 let on_connection_open_rx = on_connection_open_rx.for_each(|message| {
@@ -96,71 +152,5 @@ async fn main() {
         tokio::spawn(on_connect_future);
     }
 
-    let _ = peer::peer_create_and_listen_events(
-        &*BASE_URL,
-        &*PEER_ID,
-        true,
-        Some(on_open_tx),
-        Some(on_call_tx),
-        Some(on_connect_tx),
-        Some(on_close_tx),
-        Some(on_error_tx),
-    )
-    .await;
-}
-
-// Peer Event Callbacks
-// FIXME
-#[allow(dead_code)]
-fn on_peer_call(event: peer::formats::PeerCallEvent) -> impl Future<Output = ()> {
-    debug!("on_peer_call: {:?}", event);
-    future::ready(())
-}
-
-// FIXME
-#[allow(dead_code)]
-fn on_peer_connect(event: peer::formats::PeerConnectionEvent) -> impl Future<Output = ()> {
-    debug!("on_peer_connect: {:?}", event);
-    future::ready(())
-}
-
-// FIXME
-#[allow(dead_code)]
-fn on_peer_close(event: peer::formats::PeerCloseEvent) -> impl Future<Output = ()> {
-    debug!("on_peer_close: {:?}", event);
-    future::ready(())
-}
-
-// FIXME
-#[allow(dead_code)]
-fn on_peer_error(event: peer::formats::PeerErrorEvent) -> impl Future<Output = ()> {
-    warn!("on_peer_error: {:?}", event);
-    future::ready(())
-}
-
-// DataConnection Event Callbacks
-// FIXME
-#[allow(dead_code)]
-fn on_data_open(data_connection_id: String) -> impl Future<Output = ()> {
-    debug!("on_data_open: {:?}", data_connection_id);
-    future::ready(())
-}
-
-// FIXME
-#[allow(dead_code)]
-fn on_data_close(data_connection_id: String) -> impl Future<Output = ()> {
-    debug!("on_data_close: {:?}", data_connection_id);
-    future::ready(())
-}
-
-// FIXME
-#[allow(dead_code)]
-fn on_data_error(
-    (data_connection_id, error_message): (String, String),
-) -> impl Future<Output = ()> {
-    warn!(
-        "on_data_error: {:?} {:?}",
-        data_connection_id, error_message
-    );
-    future::ready(())
+    let _ = peer_runner(observers).await;
 }

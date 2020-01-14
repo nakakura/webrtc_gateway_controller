2949897e395e8e9271722fdc18a0af120c32afcd
Toshiya Nakakura <nakakura@me.com>
2020-01-14 23:14:20 +0900

    set base_url from outside

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,29 @@ pub mod media;
 pub mod peer;
 pub mod terminal;
 
+use std::sync::Once;
+
 use lazy_static::*;
 
-lazy_static! {
-    pub static ref BASE_URL: String =
-        ::std::env::var("BASE_URL").unwrap_or("http://localhost:8000".to_string());
+static mut BASE_URL: String = String::new();
+static INIT: Once = Once::new();
+static INIT_CHECK: Once = Once::new();
+
+pub fn initialize(base_url: impl Into<String>) {
+    unsafe {
+        INIT.call_once(|| {
+            BASE_URL = base_url.into();
+        });
+    }
 }
 
-pub fn base_url() -> &'static str {
-    &*BASE_URL
+pub (crate) fn base_url() -> &'static str {
+    unsafe {
+        INIT_CHECK.call_once(|| {
+            if BASE_URL.len() == 0 {
+                panic!("not initialized");
+            }
+        });
+        &BASE_URL
+    }
 }
